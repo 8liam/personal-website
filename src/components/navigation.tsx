e32aebde6acd728a954d2083ac236766db18634d
Liam <n11102764@qut.edu.au>
2023-11-16 02:36:11 +1000

    Added dark mode toggle

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,15 +1,23 @@
+import ThemeToggle from "./themetoggle";
 
 export default function Navigation() {
 
     return (
         <>
             <div className="navigation">
-                <a href="#intro" className="navigation-link">intro</a>
-                <a href="#about" className="navigation-link">about</a>
-                <a href="#education" className="navigation-link">education</a>
-                <a href="#projects" className="navigation-link">projects</a>
-                <a href="#contact" className="navigation-link">contact</a>
+                <div className="links">
+                    <a href="#intro" className="navigation-link">intro</a>
+                    <a href="#about" className="navigation-link">about</a>
+                    <a href="#education" className="navigation-link">education</a>
+                    <a href="#projects" className="navigation-link">projects</a>
+                    <a href="#contact" className="navigation-link">contact</a>
+                </div>
+                <div className="theme-toggler">
+                    <ThemeToggle />
+                </div>
+
             </div>
+
         </>
     )
 }
---
+++ src/components/themetoggle.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { MdOutlineNightsStay } from 'react-icons/md';
+import { FaRegLightbulb, FaLightbulb } from 'react-icons/fa';
+
+
+const ThemeToggle = () => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [isDarkMode, setIsDarkMode] = useState(prefersDarkMode || false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+        const handleChange = (event) => {
+            setIsDarkMode(event.matches);
+        };
+
+        mediaQuery.addEventListener('change', handleChange);
+
+        // Initial setup
+        document.body.classList.toggle('dark-theme', isDarkMode);
+        document.body.classList.toggle('light-theme', !isDarkMode);
+
+        return () => {
+            mediaQuery.removeEventListener('change', handleChange);
+        };
+    }, [isDarkMode]);
+
+    const toggleTheme = () => {
+        document.body.classList.toggle('dark-theme', !isDarkMode);
+        document.body.classList.toggle('light-theme', isDarkMode);
+        setIsDarkMode(!isDarkMode);
+    };
+
+    return (
+        <>
+            {isDarkMode ? (
+                <FaRegLightbulb onClick={toggleTheme} />
+
+            ) : (
+                <FaLightbulb onClick={toggleTheme} style={{ color: 'var(--link-color)' }} />
+
+            )}
+        </>
+    );
+};
+
+export default ThemeToggle;

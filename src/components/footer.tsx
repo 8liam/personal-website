dd60f232631d906818652c759beefd7ef7c03a46
Liam <64935867+8liam@users.noreply.github.com>
2023-11-15 15:08:04 +1000

    Delete src directory

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer () {
-    return(
-        <>
-        <div className="footer pixel-font">
-            <p>Designed and Built by <span className="name">Liam Grant</span></p>
-        </div>
-        </>
-    )
-}
